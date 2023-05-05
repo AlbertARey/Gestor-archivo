92d2db5edc2db83dd5740392e0a90d6b534d8224
CONTRALORIA\aalba <aalba@contraloria.gob.do>
2023-05-05 15:02:41 -0400

    Plantilla repo archivos

---
+++ vuestic-admin-master/src/pages/admin/statistics/charts/Charts.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="charts"></div>
+</template>
+
+<script setup lang="ts">
+  // import { useI18n } from 'vue-i18n'
+  // import { useChartData } from '../../../../data/charts/composables/useChartData'
+  // import {
+  //   lineChartData,
+  //   doughnutChartData,
+  //   bubbleChartData,
+  //   pieChartData,
+  //   barChartData,
+  //   horizontalBarChartData,
+  // } from '../../../../data/charts'
+  // import VaChart from '../../../../components/va-charts/VaChart.vue'
+
+  // const lineChartDataGenerated = useChartData(lineChartData, 0.7)
+  // const doughnutChartDataGenerated = useChartData(doughnutChartData)
+  // const bubbleChartDataGenerated = useChartData(bubbleChartData, 0.9)
+  // const pieChartDataGenerated = useChartData(pieChartData)
+  // const barChartDataGenerated = useChartData(barChartData)
+  // const horizontalBarChartDataGenerated = useChartData(horizontalBarChartData)
+
+  // const { t } = useI18n()
+</script>
+
+<style lang="scss">
+  .chart-widget {
+    .va-card__content {
+      height: 450px;
+    }
+  }
+</style>
